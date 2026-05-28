88203cbe48fee6b918f62086284584e161d4dfd7
Andrejs M-J <andrejs@gmail.com>
2026-05-28 21:48:46 +0300

    Enable stan on tests

--- tests/Application/TelegramNotifierTest.php
+++ tests/Application/TelegramNotifierTest.php
@@ -9,9 +9,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
-use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\HttpFactory;
-use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\RequestInterface;
@@ -21,78 +19,71 @@ final class TelegramNotifierTest extends TestCase
 {
     public function testPhotoMessageUsesSendPhotoWithCaption(): void
     {
-        $history  = [];
-        $notifier = self::notifier([new Response()], $history);
+        $requests = [];
+        $notifier = self::notifier([self::recordResponse($requests)]);
 
         $notifier->send(
             "https://www.ss.com/msg/example.html\nBrivibas\nK: 4 | m2: 90 | EUR: 250 000",
             'https://i.ss.com/gallery/example.t.jpg',
         );
 
-        self::assertCount(1, $history);
+        self::assertCount(1, $requests);
         self::assertSame(
             'https://api.telegram.org/botTOKEN/sendPhoto?chat_id=123',
-            (string) $history[0]['request']->getUri(),
+            (string) $requests[0]->getUri(),
         );
         self::assertSame(
             'photo=https%3A%2F%2Fi.ss.com%2Fgallery%2Fexample.t.jpg&caption=https%3A%2F%2Fwww.ss.com%2Fmsg%2Fexample.html%0ABrivibas%0AK%3A+4+%7C+m2%3A+90+%7C+EUR%3A+250+000',
-            (string) $history[0]['request']->getBody(),
+            (string) $requests[0]->getBody(),
         );
     }
 
     public function testPhotoFailureFallsBackToTextMessage(): void
     {
-        $history  = [];
-        $notifier = self::notifier([
-            new RequestException('Nope', new Request('POST', 'https://api.telegram.org/botTOKEN/sendPhoto?chat_id=123')),
-            new Response(),
-        ], $history);
+        $requests = [];
+        $notifier = self::notifier([self::recordFailure($requests), self::recordResponse($requests)]);
 
         $notifier->send('https://www.ss.com/msg/example.html', 'https://i.ss.com/gallery/example.t.jpg');
 
-        self::assertCount(2, $history);
+        self::assertCount(2, $requests);
         self::assertSame(
             'https://api.telegram.org/botTOKEN/sendPhoto?chat_id=123',
-            (string) $history[0]['request']->getUri(),
+            (string) $requests[0]->getUri(),
         );
         self::assertSame(
             'https://api.telegram.org/botTOKEN/sendMessage?chat_id=123',
-            (string) $history[1]['request']->getUri(),
+            (string) $requests[1]->getUri(),
         );
         self::assertSame(
             'text=https%3A%2F%2Fwww.ss.com%2Fmsg%2Fexample.html',
-            (string) $history[1]['request']->getBody(),
+            (string) $requests[1]->getBody(),
         );
     }
 
     public function testMissingImageSendsTextMessage(): void
     {
-        $history  = [];
-        $notifier = self::notifier([new Response()], $history);
+        $requests = [];
+        $notifier = self::notifier([self::recordResponse($requests)]);
 
         $notifier->send('https://www.ss.com/msg/example.html', null);
 
-        self::assertCount(1, $history);
+        self::assertCount(1, $requests);
         self::assertSame(
             'https://api.telegram.org/botTOKEN/sendMessage?chat_id=123',
-            (string) $history[0]['request']->getUri(),
+            (string) $requests[0]->getUri(),
         );
         self::assertSame(
             'text=https%3A%2F%2Fwww.ss.com%2Fmsg%2Fexample.html',
-            (string) $history[0]['request']->getBody(),
+            (string) $requests[0]->getBody(),
         );
     }
 
-    /**
-     * @param list<Response|RequestException>        $queue
-     * @param list<array{request: RequestInterface}> $history
-     */
-    private static function notifier(array $queue, array &$history): TelegramNotifier
+    /** @param list<callable> $queue */
+    private static function notifier(array $queue): TelegramNotifier
     {
         $httpFactory = new HttpFactory();
         $handler     = HandlerStack::create(new MockHandler($queue));
-        $handler->push(Middleware::history($history));
-        $httpClient = new Client(['handler' => $handler]);
+        $httpClient  = new Client(['handler' => $handler]);
 
         return new TelegramNotifier(
             'https://api.telegram.org/botTOKEN/sendMessage?chat_id=123',
@@ -101,5 +92,33 @@ final class TelegramNotifierTest extends TestCase
             $httpFactory,
             new NullLogger(),
         );
+    }
+
+    /**
+     * @param list<RequestInterface> $requests
+     *
+     * @return callable(RequestInterface): Response
+     */
+    private static function recordResponse(array &$requests): callable
+    {
+        return static function (RequestInterface $request) use (&$requests): Response {
+            $requests[] = $request;
+
+            return new Response();
+        };
+    }
+
+    /**
+     * @param list<RequestInterface> $requests
+     *
+     * @return callable(RequestInterface): RequestException
+     */
+    private static function recordFailure(array &$requests): callable
+    {
+        return static function (RequestInterface $request) use (&$requests): RequestException {
+            $requests[] = $request;
+
+            return new RequestException('Nope', $request);
+        };
     }
 }
